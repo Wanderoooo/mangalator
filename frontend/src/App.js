c3eefb6ce5eece39bb88e75417e7b22f47ddd596
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-01-20 13:29:09 -0800

    ImageRendering works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import ImageRenderer from './ImageRenderer';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  const handleImageUpload = event => {
+    let files = event.target.files;
+    let imagesArray = [...images];
+
+    for (let i = 0; i < files.length; i++) {
+      let reader = new FileReader();
+
+      reader.onloadend = () => {
+        imagesArray.push(reader.result);
+        setImages(imagesArray);
+      };
+
+      reader.readAsDataURL(files[i]);
+    }
+  };
+
+  console.log(images);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageRenderer imgs={images}/>
+      <input type="file" accept=".png, .jpg, .jpeg" multiple onChange={handleImageUpload} title="Input files here" placeholder="ohueidjsk" />
     </div>
   );
 }
