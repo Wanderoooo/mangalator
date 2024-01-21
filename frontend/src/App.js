02f759a08518f3f9e29f8ae2c555497ad4a3b3f6
Andrew Wang <andrewzlwang@outlook.com>
2024-01-20 16:56:04 -0800

    i cooked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,107 +21,14 @@ const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   const [images, setImages] = useState([]);
+  const [activeContent, setActiveContent] = useState("home"); 
 
   const handleImageUpload = event => {
     let files = Array.from(event.target.files);
     setImages(prevImages => [...prevImages, ...files]);
   };
   
-
   console.log(images);
-
-  // return (
-  //   <>
-  //   <div className="bg-white flex flex-col items-stretch">
-  //     <div className="container topbar">
-  //       <img
-  //         loading="lazy"
-  //         src={menu}
-  //         className="aspect-square overlay menu"
-  //         width='54px'
-  //         height='54px'
-  //         justify='left'
-  //       />
-  //       <img
-  //         loading="lazy"
-  //         src={mangalatorlogo}
-  //         className="aspect-square overlay logo"
-  //         width='97px'
-  //         height='97px'
-  //       />
-  //       <div className="rectangle underlay" />
-  //     </div>
-  //     <div className="absolute inset-0 bg-blue-500 opacity-50 z-0"></div>
-  //     <div className="bg-neutral-300 flex w-[296px] max-w-full flex-col items-stretch self-start">
-  //       <span className="flex justify-between gap-5 px-9 py-8 border-b-stone-400 border-b border-solid items-start max-md:px-5">
-         
-  //         <div className="text-black text-3xl">Profile</div>
-  //       </span>
-  //       <span className="flex justify-between gap-5 px-9 py-8 border-b-stone-400 border-b border-solid items-start max-md:px-5">
-  //         <img
-  //           loading="lazy"
-  //           src={user}
-  //           className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
-  //         />
-  //         <div className="text-black text-3xl self-stretch">Read</div>
-  //       </span>
-  //       <span className="flex items-center justify-between gap-5 px-9 py-6 border-b-stone-400 border-b border-solid max-md:px-5">
-  //         <img
-  //           loading="lazy"
-  //           src="https://cdn.builder.io/api/v1/image/assets/TEMP/03a31290361a5f47ac0f41e0c0a9bdbe78b68982fa9848e689d5aa576f59c070?"
-  //           className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto"
-  //         />
-  //         <div className="text-black text-3xl self-stretch">Upload</div>
-  //       </span>
-  //       <span className="flex items-stretch justify-between gap-5 px-9 py-8 border-b-stone-400 border-b border-solid max-md:px-5">
-  //         <img
-  //           loading="lazy"
-  //           src="https://cdn.builder.io/api/v1/image/assets/TEMP/c76459c04e8258ff93e3d756f963ff9366f70f60cfc94a6524871a632621971c?"
-  //           className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
-  //         />
-  //         <div className="text-black text-3xl">Search</div>
-  //       </span>
-  //       <span className="flex justify-between gap-5 mt-[473px] pt-8 pb-5 px-9 border-t-stone-400 border-t border-solid items-start max-md:mt-10 max-md:px-5">
-  //         <img
-  //           loading="lazy"
-  //           src="https://cdn.builder.io/api/v1/image/assets/TEMP/40b025eb622579bbffa84f6c997d90da6968594b2e92e6bf885df4d378254fd3?"
-  //           className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
-  //         />
-  //         <div className="text-black text-3xl">Log in</div>
-  //       </span>
-  //     </div>
-  //   </div>
-  //   <div className="App">
-  //     <ImageRenderer imgs={images}/>
-  //     <input type="file" accept=".png, .jpg, .jpeg" multiple onChange={handleImageUpload} title="Input files here" placeholder="ohueidjsk" />
-  //   </div>
-  //   <Router>
-  //   <div>
-  //     <nav>
-  //       <ul>
-  //         <li>
-  //           <Link to="/">Home</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/account">Account</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/translator">Translator</Link>
-  //         </li>
-  //       </ul>
-  //     </nav>
-
-  //     <Routes>
-  //       <Route path="/" element={<HomePage />} />
-  //       <Route path="/account" element={<AccountPage />} />
-  //       <Route path="/translator" element={<MangaPage />} />
-  //       <Route path="/login" component={LoginPage} />
-  //       <Route path="/register" component={RegisterPage} />
-  //     </Routes>
-  //   </div>
-  // </Router>
-  //   </>
-  // );
 
   return (
     <Layout className='full-height'>
@@ -135,24 +42,22 @@ function App() {
         />
       </Header>
       <Layout>
-        <Sider className='sider'>left sidebar</Sider>
-        <Content>
         <Router>
+        <Sider className='sider'>
+        <Menu className='sider'>
+          <Menu.Item key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="account">
+            <Link to="/account">Account</Link>
+          </Menu.Item>
+          <Menu.Item key="translator">
+            <Link to="/translator">Translator</Link>
+          </Menu.Item>
+        </Menu>
+        </Sider>
+        <Content className='content'>
           <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/account">Account</Link>
-                </li>
-                <li>
-                  <Link to="/translator">Translator</Link>
-                </li>
-              </ul>
-            </nav>
-
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/account" element={<AccountPage />} />
@@ -161,8 +66,12 @@ function App() {
               <Route path="/register" component={RegisterPage} />
             </Routes>
           </div>
-        </Router>
+        <div className="App">
+         <ImageRenderer imgs={images}/>
+         <input type="file" accept=".png, .jpg, .jpeg" multiple onChange={handleImageUpload} title="Input files here" placeholder="ohueidjsk" />
+        </div>
         </Content>
+        </Router>
       </Layout>
       <Footer>footer</Footer>
     </Layout>
--- frontend/src/HomeScreen/HomeScreen.js
+++ frontend/src/HomeScreen/HomeScreen.js
@@ -3,7 +3,6 @@ import Login from "../LoginScreen/Login";
 function HomeScreen() {
     return (
         <div>
-            <p>HomeScreen</p>
             <Login/>
 
         </div>
