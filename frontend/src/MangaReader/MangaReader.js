f9a881fa8f4cac1d050bc7a6f2416b9df3c43c1f
Andrew Wang <andrewzlwang@outlook.com>
2024-01-20 21:35:24 -0800

    added scroll

--- frontend/src/MangaReader/MangaReader.js
+++ frontend/src/MangaReader/MangaReader.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Carousel, Button } from 'antd';
+import { Carousel, Button, Divider } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import mangalatorlogo from '../assets/mangalatorlogo.png';
 import testmanga from '../assets/testmanga.png';
@@ -8,6 +8,17 @@ import img7035 from '../assets/IMG_7035.PNG';
 import img7036 from '../assets/IMG_7036.PNG';
 import './MangaReader.css';
 import { Flex } from 'antd';
+
+function VerticalScroll(props) {
+    return (
+      <div className="image-container">
+        <img src={img7034} alt="Image 1" />
+        <img src={img7035} alt="Image 2" />
+        <img src={img7036} alt="Image 3" />
+        {/* Add more images as needed */}
+      </div>
+    );
+}
 
 
 
@@ -23,20 +34,24 @@ export default function MangaReader(props) {
     }
   
     return (
-      <Flex style={{ flexDirection: 'row' }}>
-        <Button className="carousel-button carousel-button-left" onClick={previous} icon={<LeftOutlined />} />
-        <Carousel ref={carouselRef} className="carousel">
-          <div>
-            <img src={img7034} alt="lgo" className='cimg' />
-          </div>
-          <div>
-            <img src={img7035} alt="po" className='cimg' />
-          </div>
-          <div>
-            <img src={img7036} alt="w" className='cimg' />
-          </div>
-        </Carousel>
-        <Button className="carousel-button carousel-button-right" onClick={next} icon={<RightOutlined />}/>
-      </Flex>
+        <Flex style={{ flexDirection: 'row'}}>
+            <Flex style={{ flexDirection: 'row' }}>
+                <Button className="carousel-button carousel-button-left" onClick={previous} icon={<LeftOutlined />} />
+                <Carousel ref={carouselRef} className="carousel">
+                <div>
+                    <img src={img7034} alt="lgo" className='cimg' />
+                </div>
+                <div>
+                    <img src={img7035} alt="po" className='cimg' />
+                </div>
+                <div>
+                    <img src={testmanga} alt="w" className='cimg' />
+                </div>
+                </Carousel>
+                <Button className="carousel-button carousel-button-right" onClick={next} icon={<RightOutlined />}/>
+            </Flex>
+            <Divider type="vertical" className="divider" />
+            <VerticalScroll />
+        </Flex>
     );
   }
